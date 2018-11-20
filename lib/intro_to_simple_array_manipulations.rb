707fa8cf9478bc3148a8e4e365639da5e9a5bfee
Nick Rodriguez <nickbenrodriguez@gmail.com>
2018-11-20 02:12:47 +0000

    automatically backed up by learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,37 @@
+def using_push
+end
+
+def using_unshift
+end
+
+def using_pop
+end
+
+def pop_with_args
+end
+
+def using_shift
+end
+
+def shift_with_args
+end
+
+def using_concat
+end
+
+def using_insert
+end
+
+def using_uniq
+end
+
+def using_flatten
+end
+
+def using_delete(arr, string)
+  arr.delete(string)
+end
+
+def using_delete_at(arr, int)
+  arr.delete(int)
+end
